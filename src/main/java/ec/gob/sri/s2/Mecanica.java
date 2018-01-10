a6c194a126c4b7110b7cc0c090964f28d0754b2a
excz <excz010715@UIO11EXCZ010715.sri.ad>
2018-01-10 12:59:11 -0500

    Agregadas clases y herencia

--- src/main/java/ec/gob/sri/s2/Mecanica.java
+++ src/main/java/ec/gob/sri/s2/Mecanica.java
@@ -5,6 +5,8 @@
  */
 package ec.gob.sri.s2;
 
+import java.util.Random;
+
 /**
  *
  * @author excz010715
@@ -12,30 +14,177 @@ package ec.gob.sri.s2;
 public class Mecanica {
 
     private static String PRINT = "print";
-    private static String PRINTLN = "print";
+    private static String PRINTLN = "println";
     private static String ERROR = "error";
+    private static int PRECIO_FIJO = 30;
+    private static int PRECIO_FIJO_REVISION = 20;
+    private static int DIAS_LIMITE_REPARACION = 14;
+    private static int DIAS_LIMITE_PINTURA = 21;
+    private static int DIAS_LIMITE_REVISION = 7;
 
     public static void main(String[] args) {
-        imprimirEnPantalla(ERROR, PRINT);
+        Utilidades util = new Utilidades();
+        Trabajo trabajo = new ReparacionesMecanicas(util.generarNumero(), 0, "Reparación 1", 0, "ABIERTO", 0);
+        /*System.out.println(trabajo.getIdentificador());
+        System.out.println(trabajo.getDescripcion());
+        System.out.println(trabajo.getHoras());
+        System.out.println(trabajo.getEstado());
+        System.out.println(trabajo.getCosto());*/
+        util.imprimirEnPantalla(trabajo.getIdentificador(), PRINTLN);
+        util.imprimirEnPantalla("--------------", PRINTLN);
+        ReparacionesMecanicas trabajo1=new ReparacionesMecanicas(50, 1, "Reparaación 2", 20,"CERRADO", 100);
+        util.imprimirEnPantalla(trabajo1.getIdentificador(), PRINTLN);
+        util.imprimirEnPantalla(trabajo1.getDescripcion(), PRINTLN);
+        util.imprimirEnPantalla(trabajo1.getHoras(), PRINTLN);
+        util.imprimirEnPantalla(trabajo1.getEstado(), PRINTLN);
+        util.imprimirEnPantalla(trabajo1.calcularPrecioTotalA(), PRINTLN);
+        util.imprimirEnPantalla(trabajo1.getPrecioMaterial(), PRINTLN);
+        
+
+        
 
     }
 
-    static void imprimirEnPantalla(String mensaje, String tipo) {
-        switch (tipo) {
-            case "print":
-                System.out.print(mensaje);
-                break;
-            case "println":
-                System.out.println(mensaje);
-                break;
-            case "error":
-                System.err.println(mensaje);
-                break;                    
-            default:
-                System.out.println("No se pudo identificar el tipo de impresión solicitada");
+    static class Trabajo {
+
+        private int identificador;
+        private String descripcion;
+        private int horas;
+        private String estado;
+        private double costo;
+
+        public Trabajo() {
+
+        }
+
+        public Trabajo(int identificador, String descripcion, int horas, String estado, double costo) {
+            this.identificador = identificador;
+            this.descripcion = descripcion;
+            this.horas = horas;
+            this.estado = estado;
+            this.costo = costo;
+        }
+
+        public int getIdentificador() {
+            return identificador;
+        }
+
+        public void setIdentificador(int identificador) {
+            this.identificador = identificador;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+
+        public void setDescripcion(String descripcion) {
+            this.descripcion = descripcion;
+        }
+
+        public int getHoras() {
+            return horas;
+        }
+
+        public void setHoras(int horas) {
+            this.horas = horas;
+        }
+
+        public String getEstado() {
+            return estado;
+        }
+
+        public void setEstado(String estado) {
+            this.estado = estado;
+        }
+
+        public double getCosto() {
+            return costo;
+        }
+
+        public void setCosto(double costo) {
+            this.costo = costo;
         }
     }
-    static void generarNumero(){
+    static class Reparaciones extends Trabajo {
+
+        private double precioMaterial;
+
+        public Reparaciones(int identificador, String descripcion, int horas, String estado, double costo) {
+            super(identificador, descripcion, horas, estado, costo);
+        }
+
+        public Reparaciones(double precioMaterial, int identificador, String descripcion, int horas, String estado, double costo) {
+            super(identificador, descripcion, horas, estado, costo);
+            this.precioMaterial = precioMaterial;
+        }
+
+        public double calcularPrecioTotal() {
+            return getHoras() * PRECIO_FIJO;
+        }
+
+        public double getPrecioMaterial() {
+            return precioMaterial;
+        }
+
+        public void setPrecioMaterial(double precioMaterial) {
+            this.precioMaterial = precioMaterial;
+        }
+
+    }
+    static class ReparacionesMecanicas extends Reparaciones {
+
+        public ReparacionesMecanicas(double precioMaterial, int identificador, String descripcion, int horas, String estado, double costo) {
+            super(precioMaterial, identificador, descripcion, horas, estado, costo);
+        }
+
         
+        public double calcularPrecioTotalA() {
+            return calcularPrecioTotal() + (getPrecioMaterial() / 3);
+        }
+    }
+    static class ReparacionesChapasPintura extends Reparaciones {
+
+        public ReparacionesChapasPintura(double precioMaterial, int identificador, String descripcion, int horas, String estado, double costo) {
+            super(precioMaterial, identificador, descripcion, horas, estado, costo);
+        }
+
+        public double calcularPrecioTotalB() {
+            return calcularPrecioTotal() + (getPrecioMaterial() / 5);
+        }
+    }
+    static class Revision extends Reparaciones {
+
+        public Revision(double precioMaterial, int identificador, String descripcion, int horas, String estado, double costo) {
+            super(precioMaterial, identificador, descripcion, horas, estado, costo);
+        }
+
+        public double calcularPrecioTotalC() {
+            return PRECIO_FIJO + PRECIO_FIJO_REVISION;
+        }
+    }
+    /**
+     * Clase que aporta con métodos transversales al sistema Mecánica
+     */
+    public static class Utilidades {
+
+        void imprimirEnPantalla(Object mensaje, String tipo) {
+            switch (tipo) {
+                case "print":
+                    System.out.print(mensaje);
+                    break;
+                case "println":
+                    System.out.println(mensaje);
+                    break;
+                case "error":
+                    System.err.println(mensaje);
+                    break;
+                default:
+                    System.out.println("No se pudo identificar el tipo de impresión solicitada");
+            }
+        }
+
+        int generarNumero() {
+            return new Random().nextInt();
+        }
     }
 }
